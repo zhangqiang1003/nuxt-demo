6e0a59f44a469f684676748da90fe92a1c709882
zhangqiang <zhangqiang@qeeyou.cn>
2019-01-08 16:43:58 +0800

    [utils]-添加操作cokie的方法

--- service/utils.js
+++ service/utils.js
@@ -18,5 +18,45 @@ export default {
   checkPassword: function(pass) {
     const re = /[^a-zA-Z0-9,\.<>/?;:'\"\[\]\{\}\|\\`~!@#\$%\^&\*\(\)_\+-=]+/
     return re.test(pass) ? false : true // true - 密码合法 | false - 密码不合法
+  },
+  // 操作cookie
+  handleCookie: {
+    /**
+     * 写入cookie
+     * @param {Strig} name 
+     * @param {String} value 
+     * @param {Number} day
+     */
+    setCookie: function(name, value, day) {
+      if (day && typeof day === 'number') {
+        const expires = day * 24 * 60 * 60 *1000
+        const date = new Date(new Date() + expires)
+        document.cookie = `${name}=${escape(value)};expires=${date.toUTCString()};path=/`
+      } else {
+        document.cookie = `${name}=${escape(value)};path=/`
+      }
+    },
+    /**
+     * 获取cookie
+     * @param {String} name cookie的名称
+     * @returns {null} 不存在时，返回null 
+     */
+    getCookie: function(name) {
+      let arr = undefined
+      const reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+      arr = document.cookie.match(reg)
+      return document.cookie.match(reg) ? unescape(arr[2]) : null
+    },
+    /**
+     * 删除cookie
+     * @param {String} name 
+     */
+    delCookie: function(name) {
+      const day = -1
+      const value = ' '
+      const expires = day * 24 * 60 * 60 *1000
+      const date = new Date(new Date() + expires)
+      document.cookie = `${name}=${escape(value)};expires=${date.toUTCString()};path=/`
+    }
   }
 }
